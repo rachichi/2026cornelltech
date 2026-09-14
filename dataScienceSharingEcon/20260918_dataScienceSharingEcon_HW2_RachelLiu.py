7c4535da1e37662ae30e81d627fd57016b9def8f
rachichi <rachel.liu.1600@gmail.com>
2026-09-14 13:57:55 -0400

    Add pseudocode for arrival and departure events in HW2

--- dataScienceSharingEcon/20260918_dataScienceSharingEcon_HW2_RachelLiu.py
+++ dataScienceSharingEcon/20260918_dataScienceSharingEcon_HW2_RachelLiu.py
@@ -1,4 +1,12 @@
 # Pseudocode from HW1
 ## Say we are time t and arrival event AE occurs
+### N++
+### Sample interarrival time IA
+### Schedule arrival event AE at time t+IA
+### If (N==1) or (S1==0) then
+#### Sample service time for server 1, ST1
+#### Schedule departure event D1 at time t+ST1
+
 ## Say we are at time t and departure event D1 occurs
 ## Say we are at time t and departure event D2 occurs
+
