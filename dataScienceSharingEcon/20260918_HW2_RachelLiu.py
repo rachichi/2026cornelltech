ef0415f3e42288ed7cd2c633b7a4e7646fcbbb48
rachichi <rachel.liu.1600@gmail.com>
2026-09-15 13:14:42 -0400

    Update HW2 prompts and Python script: Clarify simulation requirements for the two-server queueing system and add necessary imports for interarrival time generation.

--- dataScienceSharingEcon/20260918_HW2_RachelLiu.py
+++ dataScienceSharingEcon/20260918_HW2_RachelLiu.py
@@ -1,4 +1,8 @@
-def interArrTime():
+import random                   # Need this to find the distributions
+import sys                      # Need this to 
+
+def generateInterarrival():     #
+    """Draw interrival time """
     return t + IA
 
 def s1Time():
